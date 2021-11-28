8cc24771186c1c3884bb9fdff7ad38ebc5db1663
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-11-28 23:40:12 +0700

    Slicing hero section

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,8 +1,26 @@
+import Image from 'next/image';
+
 const Hero = () => {
   return (
-    <>
-      <div>Hero</div>
-    </>
+    <section className="box">
+      <div className="box-inner grid-col-2 py-lg">
+        <div>
+          <h1 className="text-xl">{`Hey there, I'm Radi`}</h1>
+          <p className="text-lg">
+            I’m a Mobile Developer <br />
+            Building mobile application through Flutter and React Native
+          </p>
+        </div>
+        <div>
+          <Image
+            width={600}
+            height={490}
+            src="/static/images/blogging.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
