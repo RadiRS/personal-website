8f4a4a40b63f62afae8f15489d218c298c43c197
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-12-02 20:07:18 +0700

    Updated getStaticProps at home screen

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,14 @@ export const getStaticProps = () => {
   const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
   const userId = process.env.REACT_APP_EMAILJS_USER_ID;
 
+  const emailjs = {
+    serviceId,
+    templateId,
+    userId
+  };
+
   return {
-    props: {
-      emailjs: {
-        serviceId,
-        templateId,
-        userId
-      }
-    }
+    props: {emailjs}
   };
 };
 
