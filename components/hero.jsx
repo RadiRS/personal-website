9b8be2a83b06caf2bfce0919adb95b078add8109
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-12-07 15:17:06 +0700

    Updated link portfolio style at hero screen

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,21 +2,17 @@ import Image from 'next/image';
 import user from '../static/data/user';
 
 const Hero = () => {
-  const name = user.name.split(" ")[0];
+  const name = user.name.split(' ')[0];
 
   return (
     <section className="box">
       <div className="box-inner grid-col-2 py-lg align-center">
         <div>
           <h1 className="text-xl">{`Hey there, I'm ${name}`}</h1>
-          <p className="text-lg">
-            {user.intro}
-          </p>
-          <button className="btn button-primary mt-lg">
-            <a href="#portfolio" className="uppercase">
-              Check Out My Work
-            </a>
-          </button>
+          <p className="text-lg mb-md">{user.intro}</p>
+          <a href="#portfolio" className="btn button-primary uppercase">
+            Check Out My Work
+          </a>
         </div>
         <div>
           <Image
