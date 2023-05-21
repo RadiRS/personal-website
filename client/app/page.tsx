b919ff0b8de71ff6b6edd16f2516be9b6fa2be97
Radi <radi.rusadi.s@gmail.com>
2023-05-21 21:42:18 +0700

    Feat: Test deploy new feature

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Fresh Install NextJS 13</h1>
-      <h1>Test Update Feature & Auto Deployment</h1>
     </main>
   );
 }
