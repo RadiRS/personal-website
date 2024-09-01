f81965befef3401b61935e246be8a26791ebcc0e
Radi <radi.rusadi.s@gmail.com>
2024-09-01 15:26:34 +0700

    chore: install & setup shadcn/ui and add button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Button>Click Me</Button>
+    </div>
+  );
 };
 
 export default Home;
