1b605338b666663028f8329e1b0ba342fe3ab1d0
Radi <radi.rusadi.s@gmail.com>
2024-09-01 15:29:46 +0700

    chore: add static data

---
+++ static/data/user.js
@@ -0,0 +1,30 @@
+const user = {
+  name: 'Radi Rusadi',
+  avatar: '/static/images/profile.png',
+  intro:
+    'I’m a Mobile Developer\nBuilding mobile application through Flutter and React Native',
+  description:
+    'I’m a Mobile Developer from Indonesia.\nBuilding apps using platform like React Native and Flutter.\nI love playing guitar, swimming and basketball',
+  bio: [
+    {year: '1996', event: 'Born in Sorong, Indonesia'},
+    {
+      year: '2018',
+      event:
+        'Completed the Degree Program of Computer Science at STIKI University Malang, Java, Indonesia.'
+    },
+    {year: '2019 \n to 2022', event: 'Worked at Modal Rakyat.'}
+  ],
+  skill: {
+    programming: ['Javascript', 'Dart'],
+    framework: ['React Native', 'Flutter']
+  },
+  socials: {
+    email: 'radi.rusadi@paceprogrammer.com',
+    github: 'https://github.com/RadiRS',
+    linkedin: 'https://www.linkedin.com/in/radi-rusadi',
+    instagram: 'https://www.instagram.com/radi.rusadi.s'
+  },
+  link_cv: ''
+};
+
+export default user;
---
+++ static/markdown/otuchat.md
@@ -0,0 +1,22 @@
+# Otuchat
+
+Otuchat is a messenger application with bonus feature like payment & transactions. In the apps user can transfer money and buy Pulsa
+
+## Process
+
+Here are some of the work I've done up to the last version of the app:
+
+- Feature update profile
+- Feature access native module like camera & gallery
+- Feature QR Code generator
+- Fixed bugs and improvements
+
+## Tech
+
+Otuchat uses a number of open source projects to work properly:
+
+- [React Native](https://facebook.github.io/react-native/) - Javascript framework for mobile apps!
+- [RN Background Timer](https://github.com/ocetnik/react-native-background-timer) - React native background timer
+- [RN QR Code](https://www.npmjs.com/package/react-native-qrcode-svg) - React native qrcode genetor
+
+## Result
