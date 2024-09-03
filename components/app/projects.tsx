a38df8d65593f6886352970c62ec17a44ce87917
Radi <radi.rusadi.s@gmail.com>
2024-09-03 20:00:33 +0700

    feat: add image section at projects component

--- components/app/projects.tsx
+++ components/app/projects.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { formatDate } from '@/lib/utils';
 import { ProjectMetaData } from '@/lib/project';
@@ -16,18 +17,26 @@ const Projects = ({ data }: ProjectsProps) => {
             href={`/projects/${project.slug}`}
             className='flex flex-col justify-between gap-x-4 gap-y-1 sm:flex-row'
           >
+            {!!project.image && (
+              <Image
+                alt={project.slug}
+                src={project.image}
+                width={200}
+                height={200}
+                className='rounded-lg'
+              />
+            )}
             <div className='max-w-lg'>
               <p className='text-lg font-semibold'>{project.title}</p>
               <p className='mt-1 line-clamp-2 text-sm font-light text-muted-foreground'>
                 {project.summary}
               </p>
+              {!!project.publishedAt && (
+                <p className='mt-1 text-sm font-light'>
+                  {formatDate(project.publishedAt)}
+                </p>
+              )}
             </div>
-
-            {!!project.publishedAt && (
-              <p className='mt-1 text-sm font-light'>
-                {formatDate(project.publishedAt)}
-              </p>
-            )}
           </Link>
         </li>
       ))}
