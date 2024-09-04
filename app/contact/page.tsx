d16f93612a4b2e55f8d6ee5240cdc17bc8cfb7cd
Radi <radi.rusadi.s@gmail.com>
2024-09-04 21:02:52 +0700

    feat: add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+const ContactPage = () => {
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1 className='title mb-12'>Contact</h1>
+      </div>
+    </section>
+  );
+};
+
+export default ContactPage;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Intro, RecentProjects } from '@/components/app';
 
 const Home = () => {
   return (
-    <section className='py-24'>
+    <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
         <Intro />
         <RecentProjects />
