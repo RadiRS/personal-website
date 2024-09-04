2a6f13d87890aedd225db0dd0fe2cccf1bef0ef0
Radi <radi.rusadi.s@gmail.com>
2024-09-04 20:12:07 +0700

    feat: add post pages and lib

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,71 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
+
+import { formatDate } from '@/lib/utils';
+import { getPostById, getPosts } from '@/lib/post';
+import { MDXContent } from '@/components/app';
+
+type PostDetailPageProps = {
+  params: { slug: string };
+};
+
+const PostDetailPage = async ({ params }: PostDetailPageProps) => {
+  const { slug } = params;
+
+  const posts = await getPostById(slug);
+
+  if (!posts) return notFound();
+
+  const { metadata, content } = posts;
+  const { title, author, image, publishedAt } = metadata;
+
+  return (
+    <section className='pb-24 pt-32'>
+      <div className='container max-w-3xl'>
+        <Link
+          href='/posts'
+          className='mb-8 inline-flex items-center gap-2 text-sm font-light'
+        >
+          <ArrowLeftIcon className='h-5 w-5' />
+          <span>Back to posts</span>
+        </Link>
+
+        {!!image && (
+          <div className='relative mb-6 h-96 w-full overflow-hidden rounded-lg'>
+            <Image
+              fill
+              src={image}
+              alt={title || 'img'}
+              className='object-cover'
+            />
+          </div>
+        )}
+
+        <header>
+          <h1 className='title'>{title}</h1>
+          <p className='mt-3 text-xs text-muted-foreground'>
+            {author} / {formatDate(publishedAt ?? '')}
+          </p>
+        </header>
+
+        <main className='prose mt-16 dark:prose-invert'>
+          <MDXContent source={content} />
+        </main>
+      </div>
+    </section>
+  );
+};
+
+export const generateStaticParams = async () => {
+  const posts = await getPosts();
+
+  const slugs = posts.map(post => ({
+    slug: post.slug,
+  }));
+
+  return slugs;
+};
+
+export default PostDetailPage;
