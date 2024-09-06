102abdd6fc5f029de325290595de66e566336243
Radi <radi.rusadi.s@gmail.com>
2024-09-06 16:30:38 +0700

    style: update project image section

--- components/app/projects.tsx
+++ components/app/projects.tsx
@@ -17,15 +17,6 @@ const Projects = ({ data }: ProjectsProps) => {
             href={`/projects/${project.slug}`}
             className='flex flex-col justify-between gap-x-4 gap-y-1 sm:flex-row'
           >
-            {!!project.image && (
-              <Image
-                alt={project.slug}
-                src={project.image}
-                width={200}
-                height={200}
-                className='rounded-lg'
-              />
-            )}
             <div className='max-w-lg'>
               <p className='text-lg font-semibold'>{project.title}</p>
               <p className='mt-1 line-clamp-2 text-sm font-light text-muted-foreground'>
@@ -37,6 +28,15 @@ const Projects = ({ data }: ProjectsProps) => {
                 </p>
               )}
             </div>
+            {!!project.image && (
+              <Image
+                alt={project.slug}
+                src={project.image}
+                width={200}
+                height={200}
+                className='rounded-lg'
+              />
+            )}
           </Link>
         </li>
       ))}
