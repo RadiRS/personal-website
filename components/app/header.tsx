9c0663ddf058f68c2b27fdb6f1e6c234228d063e
Radi <radi.rusadi.s@gmail.com>
2024-09-06 16:56:27 +0700

    feat: add route styling when active

--- components/app/header.tsx
+++ components/app/header.tsx
@@ -1,8 +1,31 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
 
 import ThemeToggle from './theme-toggle';
 
+const paths = [
+  {
+    href: '/posts',
+    label: 'Posts',
+  },
+  {
+    href: '/projects',
+    label: 'Projects',
+  },
+
+  {
+    href: '/contact',
+    label: 'Contect',
+  },
+];
+
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur'>
       <nav className='container flex max-w-3xl items-center justify-between'>
@@ -12,15 +35,16 @@ const Header = () => {
           </Link>
         </div>
         <ul className='flex items-center gap-6 text-sm font-light text-muted-foreground'>
-          <li className='transition-colors hover:text-foreground'>
-            <Link href='/posts'>Posts</Link>
-          </li>
-          <li className='transition-colors hover:text-foreground'>
-            <Link href='/projects'>Projects</Link>
-          </li>
-          <li className='transition-colors hover:text-foreground'>
-            <Link href='/contact'>Contact</Link>
-          </li>
+          {paths.map(path => (
+            <li
+              key={path.href}
+              className={cn('transition-colors hover:text-foreground', {
+                'text-foreground': pathname === path.href,
+              })}
+            >
+              <Link href={path.href}>{path.label}</Link>
+            </li>
+          ))}
         </ul>
         <div>
           <ThemeToggle />
